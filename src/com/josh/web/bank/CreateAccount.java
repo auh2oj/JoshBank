73e83b8bdffb037769bcd865023645a9b5257c64
Joshua Goldwasser <spearfish@berkeley.edu>
2017-07-14 11:08:47 -0700

    changed file hierarchy and added a proper Login servlet and BankManager POJO

---
+++ src/com/josh/web/bank/CreateAccount.java
@@ -0,0 +1,37 @@
+package com.josh.web.bank;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateAccount
+ */
+@WebServlet("/CreateAccount")
+public class CreateAccount extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateAccount() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer = response.getWriter();
+		response.setContentType("text/html");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		writer.println("Username: "+username);
+		writer.print("Password: "+password);
+	}
+
+}
--- src/com/josh/web/bank/Login.java
+++ src/com/josh/web/bank/Login.java
@@ -1,6 +1,12 @@
 package com.josh.web.bank;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,23 +14,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Login
+ * Servlet implementation class BankManager
  */
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private BankManager manager;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Login() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getParameter("username");
-		request.getParameter("password");
+		String uname = request.getParameter("username");
+		String pswd = request.getParameter("password");
+		PrintWriter writer = response.getWriter();
+		RequestDispatcher view;
+		
+        manager = (BankManager) getServletContext().getAttribute("manager");
+        if (manager == null) {
+        		manager = new BankManager();
+        		getServletContext().setAttribute("manager", manager);
+        }
+		
+		if (!manager.authenticate(uname, pswd)) {
+			response.setContentType("text/html");
+			writer.println("Invalid username or password.\n\n");
+			view = request.getRequestDispatcher("/index.html");
+			view.include(request, response);
+		} else {
+			Account account = manager.getAccount(uname, pswd);
+			request.setAttribute("account", account);
+			view = request.getRequestDispatcher("/HomePage");
+			view.forward(request, response);
+		}
 	}
-
 }
